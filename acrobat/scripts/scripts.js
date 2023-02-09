9d5ae32b0f44463d24844462d83c38e9c12e5fa3
Chris Millar <cmillar@adobe.com>
2023-02-09 10:41:27 -0700

    WIP

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -2,24 +2,11 @@ import ContentSecurityPolicy from './contentSecurityPolicy/csp.js';
 
 ContentSecurityPolicy();
 
-// Add project-wide styles here.
-const STYLES = '/acrobat/styles/styles.css';
-
 /*
  * ------------------------------------------------------------
  * Edit below at your own risk
  * ------------------------------------------------------------
  */
-
-(function loadStyles() {
-  const paths = [STYLES];
-  paths.forEach((path) => {
-    const link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', path);
-    document.head.appendChild(link);
-  });
-}());
 
 (async function loadPage() {
   const dcConverterWidget = document.querySelector('.dc-converter-widget');
@@ -27,7 +14,6 @@ const STYLES = '/acrobat/styles/styles.css';
     const { default: converter } = await import('../blocks/dc-converter-widget/dc-converter-widget.js');
     converter(dcConverterWidget);
   }
-
 
   // IMS Ready
   const imsReady = setInterval(() => {
