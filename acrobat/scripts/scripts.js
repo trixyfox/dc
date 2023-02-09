eff6b0bed4c869521b8dcdb67e1c30e95eabd1e0
Chris Millar <cmillar@adobe.com>
2023-02-09 10:37:44 -0700

    bare widget starting point

--- acrobat/scripts/scripts.js
+++ acrobat/scripts/scripts.js
@@ -1,166 +1,9 @@
-/*
- * Copyright 2022 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
-import { setLibs } from './utils.js';
-import lanaLogging from './dcLana.js';
 import ContentSecurityPolicy from './contentSecurityPolicy/csp.js';
 
-// Set the CSP 
-// Send errors to LANA
 ContentSecurityPolicy();
-
-// Bowser Ready
-const bowserEle = document.createElement('script');
-bowserEle.id = 'bowserID';
-bowserEle.setAttribute('src', '/acrobat/scripts/bowser.js');
-document.head.appendChild(bowserEle);
-const bowserReady = setInterval(() => {
-  if (window.bowser) {
-    clearInterval(bowserReady);
-    const bowserIsReady = new CustomEvent('Bowser:Ready');
-    window.dispatchEvent(bowserIsReady);
-  }
-}, 100);
-console.log('setting bowser');
-
-// CLS Scripts
-const head = document.querySelector('head');
-const clsPopIn = document.createElement('link');
-clsPopIn.id = 'CLS_POPIN';
-clsPopIn.setAttribute('rel', 'stylesheet');
-clsPopIn.setAttribute('href', '/acrobat/styles/cls.css');
-head.appendChild(clsPopIn);
 
 // Add project-wide styles here.
 const STYLES = '/acrobat/styles/styles.css';
-
-// Use '/libs' if your live site maps '/libs' to milo's origin.
-const LIBS = 'https://milo.adobe.com/libs';
-
-const locales = {
-  // Americas
-  ar: { ietf: 'es-AR', tk: 'oln4yqj.css' },
-  br: { ietf: 'pt-BR', tk: 'inq1xob.css' },
-  ca: { ietf: 'en-CA', tk: 'pps7abe.css' },
-  ca_fr: { ietf: 'fr-CA', tk: 'vrk5vyv.css' },
-  cl: { ietf: 'es-CL', tk: 'oln4yqj.css' },
-  co: { ietf: 'es-CO', tk: 'oln4yqj.css' },
-  la: { ietf: 'es-LA', tk: 'oln4yqj.css' },
-  mx: { ietf: 'es-MX', tk: 'oln4yqj.css' },
-  pe: { ietf: 'es-PE', tk: 'oln4yqj.css' },
-  '': { ietf: 'en-US', tk: 'hah7vzn.css' },
-  // EMEA
-  africa: { ietf: 'en', tk: 'pps7abe.css' },
-  be_fr: { ietf: 'fr-BE', tk: 'vrk5vyv.css' },
-  be_en: { ietf: 'en-BE', tk: 'pps7abe.css' },
-  be_nl: { ietf: 'nl-BE', tk: 'cya6bri.css' },
-  cy_en: { ietf: 'en-CY', tk: 'pps7abe.css' },
-  dk: { ietf: 'da-DK', tk: 'aaz7dvd.css' },
-  de: { ietf: 'de-DE', tk: 'vin7zsi.css' },
-  ee: { ietf: 'et-EE', tk: 'aaz7dvd.css' },
-  es: { ietf: 'es-ES', tk: 'oln4yqj.css' },
-  fr: { ietf: 'fr-FR', tk: 'vrk5vyv.css' },
-  gr_en: { ietf: 'en-GR', tk: 'pps7abe.css' },
-  ie: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  il_en: { ietf: 'en-IL', tk: 'pps7abe.css' },
-  it: { ietf: 'it-IT', tk: 'bbf5pok.css' },
-  lv: { ietf: 'lv-LV', tk: 'aaz7dvd.css' },
-  lt: { ietf: 'lt-LT', tk: 'aaz7dvd.css' },
-  lu_de: { ietf: 'de-LU', tk: 'vin7zsi.css' },
-  lu_en: { ietf: 'en-LU', tk: 'pps7abe.css' },
-  lu_fr: { ietf: 'fr-LU', tk: 'vrk5vyv.css' },
-  hu: { ietf: 'hu-HU', tk: 'aaz7dvd.css' },
-  mt: { ietf: 'en-MT', tk: 'pps7abe.css' },
-  mena_en: { ietf: 'en', tk: 'pps7abe.css' },
-  nl: { ietf: 'nl-NL', tk: 'cya6bri.css' },
-  no: { ietf: 'no-NO', tk: 'aaz7dvd.css' },
-  pl: { ietf: 'pl-PL', tk: 'aaz7dvd.css' },
-  pt: { ietf: 'pt-PT', tk: 'inq1xob.css' },
-  ro: { ietf: 'ro-RO', tk: 'aaz7dvd.css' },
-  sa_en: { ietf: 'en', tk: 'pps7abe.css' },
-  ch_de: { ietf: 'de-CH', tk: 'vin7zsi.css' },
-  si: { ietf: 'sl-SI', tk: 'aaz7dvd.css' },
-  sk: { ietf: 'sk-SK', tk: 'aaz7dvd.css' },
-  ch_fr: { ietf: 'fr-CH', tk: 'vrk5vyv.css' },
-  fi: { ietf: 'fi-FI', tk: 'aaz7dvd.css' },
-  se: { ietf: 'sv-SE', tk: 'fpk1pcd.css' },
-  ch_it: { ietf: 'it-CH', tk: 'bbf5pok.css' },
-  tr: { ietf: 'tr-TR', tk: 'aaz7dvd.css' },
-  ae_en: { ietf: 'en', tk: 'pps7abe.css' },
-  uk: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  at: { ietf: 'de-AT', tk: 'vin7zsi.css' },
-  cz: { ietf: 'cs-CZ', tk: 'aaz7dvd.css' },
-  bg: { ietf: 'bg-BG', tk: 'aaz7dvd.css' },
-  ru: { ietf: 'ru-RU', tk: 'aaz7dvd.css' },
-  ua: { ietf: 'uk-UA', tk: 'aaz7dvd.css' },
-  il_he: { ietf: 'he', tk: 'nwq1mna.css' },
-  ae_ar: { ietf: 'ar', tk: 'nwq1mna.css' },
-  mena_ar: { ietf: 'ar', tk: 'dis2dpj.css' },
-  sa_ar: { ietf: 'ar', tk: 'nwq1mna.css' },
-  // Asia Pacific
-  au: { ietf: 'en-AU', tk: 'pps7abe.css' },
-  hk_en: { ietf: 'en-HK', tk: 'pps7abe.css' },
-  in: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  id_id: { ietf: 'id', tk: 'czc0mun.css' },
-  id_en: { ietf: 'en', tk: 'pps7abe.css' },
-  my_ms: { ietf: 'ms', tk: 'sxj4tvo.css' },
-  my_en: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  nz: { ietf: 'en-GB', tk: 'pps7abe.css' },
-  ph_en: { ietf: 'en', tk: 'pps7abe.css' },
-  ph_fil: { ietf: 'fil-PH', tk: 'ict8rmp.css' },
-  sg: { ietf: 'en-SG', tk: 'pps7abe.css' },
-  th_en: { ietf: 'en', tk: 'pps7abe.css' },
-  in_hi: { ietf: 'hi', tk: 'aaa8deh.css' },
-  th_th: { ietf: 'th', tk: 'aaz7dvd.css' },
-  cn: { ietf: 'zh-CN', tk: 'puu3xkp' },
-  hk_zh: { ietf: 'zh-HK', tk: 'jay0ecd' },
-  tw: { ietf: 'zh-TW', tk: 'jay0ecd' },
-  jp: { ietf: 'ja-JP', tk: 'dvg6awq' },
-  kr: { ietf: 'ko-KR', tk: 'qjs5sfm' },
-  // Langstore Support.
-  langstore: { ietf: 'en-US', tk: 'hah7vzn.css' }, 
-  // geo expansion MWPW-124903
-  za: { ietf: 'en-GB', tk: 'pps7abe.css' }, // South Africa (GB English)
-  ng: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Nigeria (GB English)
-  cr: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Costa Rica (Spanish Latin America)
-  ec: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Ecuador (Spanish Latin America)
-  pr: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Puerto Rico (Spanish Latin America)
-  gt: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Guatemala (Spanish Latin America)
-  eg_ar: { ietf: 'ar', tk: 'nwq1mna.css' }, // Egypt (Arabic)
-  kw_ar: { ietf: 'ar', tk: 'nwq1mna.css' }, // Kuwait (Arabic)
-  qa_ar: { ietf: 'ar', tk: 'nwq1mna.css' }, // Quatar (Arabic)
-  eg_en: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Egypt (GB English)
-  kw_en: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Kuwait (GB English)
-  qa_en: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Qatar (GB English)
-  gr_el: { ietf: 'el', tk: 'fnx0rsr.css' }, // Greece (Greek) 
-};
-
-// Add any config options.
-const CONFIG = {
-  codeRoot: '/acrobat',
-  contentRoot: '/acrobat',
-  imsClientId: 'acrobatmilo',
-  local: { edgeConfigId: 'da46a629-be9b-40e5-8843-4b1ac848745c' },
-  stage: { edgeConfigId: 'da46a629-be9b-40e5-8843-4b1ac848745c' },
-  live: { edgeConfigId: 'da46a629-be9b-40e5-8843-4b1ac848745c' },
-  prod: { edgeConfigId: '9f3cee2b-5f73-4bf3-9504-45b51e9a9961' },
-  locales,
-};
-
-// Default to loading the first image as eager.
-(async function loadLCPImage() {
-  const lcpImg = document.querySelector('img');
-  lcpImg?.setAttribute('loading', 'eager');
-}());
 
 /*
  * ------------------------------------------------------------
@@ -168,11 +11,8 @@ const CONFIG = {
  * ------------------------------------------------------------
  */
 
-const miloLibs = setLibs(LIBS);
-
 (function loadStyles() {
-  const paths = [`${miloLibs}/styles/styles.css`];
-  if (STYLES) { paths.push(STYLES); }
+  const paths = [STYLES];
   paths.forEach((path) => {
     const link = document.createElement('link');
     link.setAttribute('rel', 'stylesheet');
@@ -182,12 +22,12 @@ const miloLibs = setLibs(LIBS);
 }());
 
 (async function loadPage() {
-  const { loadArea, loadDelayed, setConfig, loadLana } = await import(`${miloLibs}/utils/utils.js`);
-  setConfig({ ...CONFIG, miloLibs });
-  await loadArea();
-  loadDelayed();
-  loadLana({ clientId: 'dxdc' });
-  lanaLogging();
+  const dcConverterWidget = document.querySelector('.dc-converter-widget');
+  if (dcConverterWidget) {
+    const { default: converter } = await import('../blocks/dc-converter-widget.js');
+    converter(dcConverterWidget);
+  }
+
 
   // IMS Ready
   const imsReady = setInterval(() => {
