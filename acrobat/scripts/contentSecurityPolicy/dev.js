70e82b50189aae6c2d181d332ee1b14e635cd4f7
draganatrajkovic <65951679+draganatrajkovic@users.noreply.github.com>
2023-03-22 15:47:38 +0100

    Update dev.js

--- acrobat/scripts/contentSecurityPolicy/dev.js
+++ acrobat/scripts/contentSecurityPolicy/dev.js
@@ -43,6 +43,7 @@ const connectSrc = [
   'main--milo--adobecom.hlx.live',
   'fast-track--milo--adobecom.hlx.page',
   'fast-track--milo--adobecom.hlx.live',
+  'cdn.linkedin.oribi.io/partner/872/domain/*',
   '*.hlx.page',
   ';',
 ];
